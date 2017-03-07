93a861139f223868d65522b3b56dea653e85d812
Detlew <detlewlabes@gmx.de>
2017-03-07 11:03:51 +0100

    More on optimize power.scABEL.sdsims() and documentation of that function

--- R/power_scABEL_sdsims.R
+++ R/power_scABEL_sdsims.R
@@ -7,14 +7,14 @@
 # ----------------------------------------------------------------------------
 power.scABEL.sdsims <- function(alpha=0.05, theta1, theta2, theta0, CV, n,   
                                 design=c("2x3x3", "2x2x4", "2x2x3"), regulator,
-                                nsims=1E5, details=FALSE, setseed=TRUE, progress,
-                                fitmethod=c(".lm.fit", "lm.fit", "fastlm", "qr"))
+                                nsims=1E5, details=FALSE, setseed=TRUE, progress)
 {
-  fitm <- match.arg(fitmethod)
+  # auto: set this to .lm.fit if n<18
+  fitm <- "qr"
   
   if(missing(progress)) {
     progress <- FALSE
-    if(nsims>1E4) progress <- TRUE
+    if(nsims>5E4) progress <- TRUE
   }
   # check if on windows, if not don't show progressbar TODO
   
@@ -87,7 +87,9 @@ power.scABEL.sdsims <- function(alpha=0.05, theta1, theta2, theta0, CV, n,
   df <- eval(dfe)
   dfRR <- eval(dfRRe)
   if(desi=="2x2x3") dfRR <- nv[2]-1 
-
+  
+  if(n<=18) fitm <- ".lm.fit"
+  
   # call the working horse
   pwr <- .pwr.ABEL.sdsims(seqs=seqs, nseq=nv, ldiff=log(theta0), s2WR=s2wR, 
                           s2WT=s2wT, C2=C2, df=df, dfRR=dfRR, nsims=nsims, 
@@ -116,20 +118,21 @@ power.scABEL.sdsims <- function(alpha=0.05, theta1, theta2, theta0, CV, n,
   # paranoia
   if(is.null(pe_constr)) pe_constr <- TRUE
   
-  if(progress) pb <- winProgressBar(title = "sims progress", min = 0, max = nsims, width = 400)
+  if(progress) pb <- txtProgressBar( min = 0, max = nsims, style = 3)
 
   if(setseed) set.seed(123456)
 #  set.seed(146389) # seed for the scripts in directory /workspace/replicate_simul
   
   tcrit <- qt(1-alpha, df)
   
+  # prep_data2 gives all logvals = 0 back
   data <- prep_data2(seqs, nseq, muR=log(10), ldiff=ldiff, s2wT=s2WT, s2wR=s2WR)
   data$tmt     <- as.factor(data$tmt)
   data$period  <- as.factor(data$period)
   data$subject <- as.factor(data$subject)
-  # measurements under treatments, values to simulate
   # change coding to 1=R, 2=T
   data_tmt <- as.numeric(data$tmt)
+  # measurements under treatments, values to simulate
   nT <- length(data_tmt[data_tmt==2])
   nR <- length(data_tmt[data_tmt==1])
   data$logval  <- sim_data2_y2(data_tmt=data_tmt, nT=nT, nR=nR, ldiff=ldiff, 
@@ -152,68 +155,15 @@ power.scABEL.sdsims <- function(alpha=0.05, theta1, theta2, theta0, CV, n,
   s2switch <- CV2mse(CVswitch)
   s2cap    <- CV2mse(CVcap)
   
-  # reserve space for the results of the simulation loop
+  # allocate memory space for the results of the simulation loop
   pes   <- vector(mode="numeric", length=nsims)
   mses  <- vector(mode="numeric", length=nsims)
   s2wRs <- vector(mode="numeric", length=nsims)
+  
+  # only ".lm.fit" and "qr" retained. if n<=18 ".lm.fit" is used else "qr"
   # programming each fitmethod in own loop to avoid 1 Mio if's. but this give 
   # no notable difference in run-time
-  if(fitmethod=="fastlm"){
-    # or better work in chunks?
-    for(j in 1:nsims){
-      # use RcppEigen
-      # Ben's method=2L gives different results to lm.fit() for
-      # design="2x2x4" and "2x2x3"
-      # but method=0L has no big advantage compared to lm.fit()
-      model   <- RcppEigen::fastLmPure(X=mm, y=logval, method=2L)
-      mses[j] <- (model$s)^2
-      pes[j]  <- model$coefficients["tmtT"]
-      
-      # R data only, rank deficient in case of design="2x2x4" and "2x2x3"?
-      modelR   <- RcppEigen::fastLmPure(X=mmR, y=logvalR, method=0L)
-      s2wRs[j] <- (modelR$s)^2
-      
-      # show progress
-      if(progress){
-        if(100*trunc(j/100)==j) 
-          setWinProgressBar(pb, j, title=paste(j, "sims done (",
-                                               trunc(1000*j/nsims)/10,"%)"))
-      }  
-      # simulate for next step
-      # this gives one step more as necessary but avoiding here an if may have some
-      # run-time advantage
-      logval  <- sim_data2_y2(data_tmt=data_tmt, nT=nT, nR=nR, ldiff=ldiff, 
-                              s2wT=s2WT, s2wR=s2WR)
-      logvalR <- logval[data_tmt == 1]
-    }  
-  } else if(fitmethod=="lm.fit") {
-    # using lm.fit()
-    for(j in 1:nsims){
-      # determine pe, mse of all data
-      model   <- lm.fit(x=mm, y=logval)
-      # lm.fit does'nt have anova() and confint() methods
-      # thus we have to calculate them explicite
-      mses[j] <- sum(model$residuals^2)/model$df.residual
-      pes[j]  <- coef(model)["tmtT"]
-      
-      # determine s2wR from R data only
-      modelR   <- lm.fit(x=mmR, y=logvalR)
-      s2wRs[j] <- sum(modelR$residuals^2)/modelR$df.residual
-      
-      # show progress
-      if(progress){
-        if(100*trunc(j/100)==j) 
-          setWinProgressBar(pb, j, title=paste(j, "sims done (",
-                                               trunc(1000*j/nsims)/10,"%)"))
-      }  
-      # simulate for next step
-      # this gives one step more as necessary but avoiding here an if may have some
-      # run-time advantage
-      logval  <- sim_data2_y2(data_tmt=data_tmt, ldiff=ldiff, nT=nT, nR=nR, 
-                              s2wT=s2WT, s2wR=s2WR)
-      logvalR <- logval[data_tmt == 1]
-    }  
-  } else if(fitmethod==".lm.fit") {
+  if(fitmethod==".lm.fit") {
     # using lm.fit()
     for(j in 1:nsims){
       # determine pe, mse of all data
@@ -229,9 +179,7 @@ power.scABEL.sdsims <- function(alpha=0.05, theta1, theta2, theta0, CV, n,
       
       # show progress
       if(progress){
-        if(100*trunc(j/100)==j) 
-          setWinProgressBar(pb, j, title=paste(j, "sims done (",
-                                               trunc(1000*j/nsims)/10,"%)"))
+        if(100*trunc(j/100)==j) setTxtProgressBar(pb, j)
       }  
       # simulate for next step
       # this gives one step more as necessary but avoiding here an if may have some
@@ -269,9 +217,7 @@ power.scABEL.sdsims <- function(alpha=0.05, theta1, theta2, theta0, CV, n,
       
       # show progress
       if(progress){
-        if(100*trunc(j/100)==j) 
-          setWinProgressBar(pb, j, title=paste(j, "sims done (",
-                                               trunc(1000*j/nsims)/10,"%)"))
+        if(100*trunc(j/100)==j) setTxtProgressBar(pb, j)
       }  
       # simulate for next step
       # this gives one step more as necessary but avoiding here an if may have some
