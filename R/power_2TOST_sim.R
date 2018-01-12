77d04b946a3afc917987507e8c955b0140999c41
Detlew <detlewlabes@gmx.de>
2018-01-12 20:09:53 +0100

    More programming on power.2TOST.sim() to make the interface backwards compatible with power.2TOST()

--- R/power_2TOST_sim.R
+++ R/power_2TOST_sim.R
@@ -4,49 +4,79 @@
 # Author D.L.
 # --------------------------------------------------------------------------
 
-power.2TOST.sim <- function(alpha=c(0.05,0.05), logscale=TRUE, CV, rho=0, n, 
-                            theta0, theta, design="2x2", robust=FALSE,
-                            nsims, setseed=TRUE, details=FALSE)
+power.2TOST.sim <- function(alpha=c(0.05,0.05), logscale=TRUE, theta0, 
+                            theta1, theta2,  CV, n, rho, design="2x2", 
+                            robust=FALSE, nsims, setseed=TRUE, details=FALSE)
 {
+  # Args:
+  #   alpha: Vector of one-sided alpha levels for each procedure
+  #   logscale: logical; if TRUE, log-transformed data is assumed
+  #   theta0: Vector of 'true' assumed ratio of geometric means
+  #   theta1: Vector of lower (bio-)equivalence limits
+  #   theta2: Vector of upper (bio-)equivalence limits
+  #   CV: Vector of coefficient of variations (use e.g. 0.3 for 30%)
+  #       for the two metrics 
+  #   n: For balanced allocation to treatment/sequence groups n is the total 
+  #      sample size. For unbalanced allocation n is a vector with sample size
+  #      per treatment/sequence group (e.g. n = c(8, 10) for an unbalanced
+  #      2x2 design with total sample size 18)
+  #   rho: Correlation between the two parameters under consideration
+  #   design: Character string describing the study design
+  #   robust: logical; if TRUE, use robust degrees of freedom (n - #sequences)
+  #   setseed: pmvt() is based on randomized quasi Monte Carlo methods.
+  #            Set seed value for (pseudo) random number generator?
+  #   t1e: logical; if TRUE, Type I Error will be calculated
+  #   details: logical; if TRUE, return P(Type I error) for all intersection
+  #            null sets, max P(Type I error) otherwise
+  if(length(alpha)==1) {
+    alpha <- rep(alpha, 2)
+    message("Scalar alpha used for both metrics.")
+  }
+  # is length(alpha=2) an real option
+  # never heard that two parallel TOST's were done with different alpha
+  if(length(alpha) != 2) stop("alpha must have two elements.")
+  
+  if (!missing(theta0) && length(theta0) != 2)
+    stop("Two theta0 values must be given!")
+  if (!missing(theta1) && length(theta1) != 2)
+    stop("Two theta1 values must be given!")
+  if (!missing(theta2) && length(theta2) != 2)
+    stop("Two theta2 values must be given!")
+  
+  if (logscale) {
+    if (missing(theta0))  theta0 <- c(0.95, 0.95)
+    if (any(theta0 <= 0)) stop("theta0 must be > 0.")
+    if (missing(theta1))  theta1 <- c(0.8, 0.8)
+    if (missing(theta2))  theta2 <- 1/theta1
+    if (any(theta1 <= 0) || any(theta1 > theta2))
+      stop("theta1 and/or theta2 not correctly specified.")
+  } else {
+    if (missing(theta0)) theta0 <- c(0.05, 0.05)
+    if (missing(theta1)) theta1 <- c(-0.2, -0.2)
+    if (missing(theta2)) theta2 <- -theta1
+    if (any(theta1 > theta2))
+      stop("theta1 and/or theta2 not correctly specified.")
+  }
+  theta <- cbind(theta1, theta2)
+    
   if(missing(CV))  stop("CVs must be given.")
   else {
     if(length(CV)==1) CV <- rep(CV,2)
     if(length(CV)!=2) stop("CV must have 2 elements.")
     if(any(CV<=0))   stop("CVs must be >0.")
   }
+  
   if(missing(n)) stop("Number of subjects must be given.")
 
-  if(length(alpha)==1) {
-    alpha <- rep(alpha, 2)
-    message("Scalar alpha used for both metrics.")
-  }
-  if(length(alpha) != 2) stop("alpha must have two elements.")
+  # correlation
+  if (missing(rho))
+    stop("Correlation between the two endpoints must be given!")
+  if (length(rho) != 1)
+    stop("One rho must be given!")
+  if (rho <= -1 || rho > 1)
+    stop("Correlation must be > -1 and < 1.")
 
-  # 'true' GMR or difference  
-  if(missing(theta0)){
-    if(logscale){
-      theta0 <- rep(0.95, 2)
-    } else {
-      theta0 <- rep(0.05, 2)
-    }
-  } else {
-    if(length(theta0)==1) theta0 <- rep(theta0, 2)
-    if(length(theta0)!=2) stop("theta0 must have 2 elements.")
-    # more to check???
-    
-  }
-  
-  # BE acceptance limits, row 1 for 1. metric, row 2 for second
-  if( missing(theta)) {
-    if(logscale){
-      theta <- matrix(c(0.8, 0.8, 1.25, 1.25), nrow=2)
-    } else {
-      theta <- matrix(c(-0.2, -0.2, 0.2, 0.2), nrow=2)
-    }
-    # more to check?
-  }
-
-  # design characteristics
+    # design characteristics
   # check if design is implemented
   d.no <- .design.no(design)
   if (is.na(d.no)) stop("Design ",design, " unknown!", call.=FALSE)
@@ -76,23 +106,22 @@ power.2TOST.sim <- function(alpha=c(0.05,0.05), logscale=TRUE, CV, rho=0, n,
   }
   nc <- sum(1/n)
   n  <- sum(n)
-  #Cfact <- sqrt(ades$bkni * nc)
+  # Cfact*mse gives the variance of the mean difference
   Cfact <- ades$bkni * nc
-  
-  #if(rho!=0) warning("rho != 0 is only experimental.", call. = FALSE)
-  stopifnot(length(rho)==1, rho >= -1, rho <= 1)
   
   if(missing(nsims)){
     nsims <- 1E5
     if(any(theta0<=theta[, 1]) | any(theta0>=theta[,2])) nsims <- 1E6
   }
   
+  # ---------------------------------------------------------------------------
+  # now the calculations are starting
   # start timer
   ptm  <- proc.time()
   
   if (setseed) set.seed(1234567)
   
-  sigma <- matrix(0,2,2)
+  sigma <- matrix(0, nrow=2, ncol=2)
   if(logscale) {
     ltheta0 <- log(theta0)
     ltheta  <- log(theta)
@@ -110,50 +139,64 @@ power.2TOST.sim <- function(alpha=c(0.05,0.05), logscale=TRUE, CV, rho=0, n,
   # cave sqrt() in case of rho<0
   s2m   <- sigma*Cfact
   
-  pes  <- matrix(0, nrow=nsims, ncol=2)
-  mses <- matrix(0, nrow=nsims, ncol=2)
-  if (rho==0){
-    # simulate point est. via normal distribution
-    pes[ , 1] <- rnorm(n=nsims, mean=ltheta0[1], sd=sqrt(s2m[1,1])) # metric 1, f.i. AUC
-    pes[ , 2] <- rnorm(n=nsims, mean=ltheta0[2], sd=sqrt(s2m[2,2])) # metric 2, f.i. Cmax
-    # simulate mse via chi-squared distribution
-    mses[ , 1] <- sigma[1,1]*rchisq(n=nsims, df=df)/df
-    mses[ , 2] <- sigma[2,2]*rchisq(n=nsims, df=df)/df
-  } else {
-    # multivariate normal with rho
-    # TODO: check this
-    pes <- mvtnorm::rmvnorm(nsims, mean=ltheta0, sigma=s2m)
-    # simulate covariance matrices via Wishart distribution
-    #or do we have here df=n-1?
-    covm      <- rWishart(n=nsims, df=df, Sigma=sigma)/df 
-    mses[, 1] <- covm[1, 1, ]
-    mses[, 2] <- covm[2, 2, ]
-    # take care of memory
-    rm(covm)
-  }
-  #browser()
-  
-  BE <- function(nu)
-  {
-    # nu = number of the metric
-    hw <- tvals[nu]*sqrt(Cfact*mses[, nu])
-    loCL <- pes[, nu] - hw
-    upCL <- pes[, nu] + hw
-    BE <- loCL >= ltheta[nu, 1] & upCL <= ltheta[nu, 2]
-    BE
-  }
-  # make BE decision for both metrics
-  BE_m1 <- BE(nu=1)
-  BE_m2 <- BE(nu=2)
-  
-  # overall BE 
-  BE <- BE_m1 & BE_m2
+  # to avoid memory problems
+  chunksize <- 1e7
+  chunks    <- 1
+  nsi       <- nsims
+  if(nsims > chunksize) {
+     chunks <- ceiling(nsims/chunksize)
+     nsi    <- chunksize
+  } 
+  BE <- 0
+  # over chunks of 1E7 if nsims > 1E7 to avoid memory problems
+  for (iter in 1:chunks){
+    # if chunks*1E7 >nsims correct nsi to given nsims
+    if(iter==chunks & chunks>1) nsi <- nsims-(chunks-1)*nsi
+    pes  <- matrix(0, nrow=nsi, ncol=2)
+    mses <- matrix(0, nrow=nsi, ncol=2)
+    # next: if directive with rho==0 was only for comparative purposes
+    # no need to have it. multivariate NV and Wishart can handle this case
+    # but retained by reason of an eventual speed gain
+    if (rho==0){
+      # simulate point est. via 2 independen normal distributions
+      pes[ , 1] <- rnorm(n=nsi, mean=ltheta0[1], sd=sqrt(s2m[1,1])) # metric 1, f.i. AUC
+      pes[ , 2] <- rnorm(n=nsi, mean=ltheta0[2], sd=sqrt(s2m[2,2])) # metric 2, f.i. Cmax
+      # simulate mse via 2 independent chi-squared distributions
+      mses[ , 1] <- sigma[1,1]*rchisq(n=nsi, df=df)/df
+      mses[ , 2] <- sigma[2,2]*rchisq(n=nsi, df=df)/df
+    } else {
+      # pes multivariate normal with rho
+      pes <- mvtnorm::rmvnorm(nsi, mean=ltheta0, sigma=s2m)
+      # simulate covariance matrices via Wishart distribution
+      covm      <- stats::rWishart(n=nsi, df=df, Sigma=sigma)/df 
+      mses[, 1] <- covm[1, 1, ]
+      mses[, 2] <- covm[2, 2, ]
+      # take care of memory
+      rm(covm)
+    } 
 
+    BE_test <- function(nu)
+    {
+      # nu = number of the metric
+      hw <- tvals[nu]*sqrt(Cfact*mses[, nu])
+      loCL <- pes[, nu] - hw
+      upCL <- pes[, nu] + hw
+      BE <- loCL >= ltheta[nu, 1] & upCL <= ltheta[nu, 2]
+      BE
+    }
+    # make BE decision for both metrics
+    BE_m1 <- BE_test(nu=1)
+    BE_m2 <- BE_test(nu=2)
+    
+    # overall BE 
+    BE <- BE + sum(BE_m1 & BE_m2)
+  } # end over chunks
+  
   if(details){
     cat(nsims, "simulations. Time consumed (secs)\n")
     print(proc.time()-ptm)
     cat("\n")
   }
-  return(sum(BE)/nsims)
+  return(BE/nsims)
   
 } #end function
