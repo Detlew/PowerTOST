641af52e4e58f7f1107028d4858942c8d898af8e
Benjamin Lang <benjamin_lang@icloud.com>
2018-01-23 20:03:04 +0100

    Change names for 2TOST

--- R/power_type1_2TOST.R
+++ R/power_type1_2TOST.R
@@ -1,35 +1,39 @@
-## Benjamin Lang
-power.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1, 
-                            theta2, theta0, CV, n, rho, design = "2x2", 
-                            robust = FALSE, setseed = TRUE) {
-  prob.2TOST.old(alpha = alpha, logscale = logscale, theta1 = theta1, 
-                 theta2 = theta2, theta0 = theta0, CV = CV, n = n, rho = rho, 
-                 design = design, robust = robust, setseed = TRUE)
+# --------------------------------------------------------------------------
+# power (or alpha) of 2 TOST via simulations
+#
+# Author D.L.
+# --------------------------------------------------------------------------
+power.2TOST <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta0, theta1,
+                        theta2, CV, n, rho, design = "2x2", robust = FALSE, 
+                        nsims, setseed = TRUE, details = FALSE) {
+  prob.2TOST(alpha = alpha, logscale = logscale, theta0 = theta0, 
+             theta1 = theta1, theta2 = theta2, CV = CV, n = n, rho = rho, 
+             design = design, robust = robust, nsims = nsims, setseed = setseed,
+             details = details)
 }
 
-type1error.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1, 
-                                 theta2, CV, n, rho, design = "2x2", robust = FALSE, 
-                                 setseed = TRUE, details = FALSE) {
-  prob.2TOST.old(alpha = alpha, logscale = logscale, theta1 = theta1, 
-                 theta2 = theta2, CV = CV, n = n, rho = rho, design = design, 
-                 robust = robust, setseed = TRUE, t1e = TRUE, details = details)
+type1error.2TOST <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta0,
+                             theta1, theta2, CV, n, rho, design = "2x2", 
+                             robust = FALSE, nsims, setseed = TRUE, 
+                             details = FALSE) {
+  prob.2TOST(alpha = alpha, logscale = logscale, theta0 = theta0, 
+             theta1 = theta1, theta2 = theta2, CV = CV, n = n, rho = rho, 
+             design = design, robust = robust, nsims = nsims, setseed = setseed,
+             t1e = TRUE, details = details)
 }
 
-prob.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1, 
-                           theta2, theta0, CV, n, rho, design = "2x2", 
-                           robust = FALSE, setseed = TRUE, t1e = FALSE, 
-                           details = FALSE) {
-  # Computes Power or Type I Error rate for two simultaneous TOST procedures,
-  # where the two parameters of the two TOSTs are correlated with correlation
-  # coefficient rho.
-  #
+prob.2TOST <- function(alpha=c(0.05,0.05), logscale=TRUE, theta0, theta1,
+                       theta2, CV, n, rho, design="2x2", robust=FALSE, nsims, 
+                       setseed=TRUE, t1e = FALSE, details=FALSE)
+{
   # Args:
   #   alpha: Vector of one-sided alpha levels for each procedure
   #   logscale: logical; if TRUE, log-transformed data is assumed
+  #   theta0: Vector of 'true' assumed ratio of geometric means
   #   theta1: Vector of lower (bio-)equivalence limits
   #   theta2: Vector of upper (bio-)equivalence limits
-  #   theta0: Vector of 'true' assumed ratio of geometric means
   #   CV: Vector of coefficient of variations (use e.g. 0.3 for 30%)
+  #       for the two metrics 
   #   n: For balanced allocation to treatment/sequence groups n is the total 
   #      sample size. For unbalanced allocation n is a vector with sample size
   #      per treatment/sequence group (e.g. n = c(8, 10) for an unbalanced
@@ -42,58 +46,21 @@ prob.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1,
   #   t1e: logical; if TRUE, Type I Error will be calculated
   #   details: logical; if TRUE, return P(Type I error) for all intersection
   #            null sets, max P(Type I error) otherwise
-  #
-  # Returns:
-  #   Type I Error if theta0 contains theta1 or theta2, Power otherwise
-  #
-  # Error handling
-  if (length(alpha) != 2)
-    stop("Two alpha values must be given!")
+  if(length(alpha)==1) {
+    alpha <- rep(alpha, 2)
+    message("Scalar alpha used for both metrics.")
+  }
+  # is length(alpha=2) an real option
+  # never heard that two parallel TOST's were done with different alpha
+  if(length(alpha) != 2) stop("alpha must have two elements.")
+  
   if (!missing(theta0) && length(theta0) != 2)
     stop("Two theta0 values must be given!")
   if (!missing(theta1) && length(theta1) != 2)
     stop("Two theta1 values must be given!")
   if (!missing(theta2) && length(theta2) != 2)
     stop("Two theta2 values must be given!")
-  if (missing(CV)) 
-    stop("CV must be given!")
-  if (length(CV) != 2)
-    stop("Two CVs must be given!")
-  if (any(CV <= 0))
-    stop("CV must be > 0")
-  if (missing(n)) 
-    stop("Number of subjects n must be given!")
-  if (missing(rho))
-    stop("Correlation between the two endpoints must be given!")
-  if (length(rho) != 1)
-    stop("One rho must be given!")
-  if (rho < -1 || rho > 1)
-    stop("Correlation must be >= -1 and =< 1.")
-  d.no <- .design.no(design)
-  if (is.na(d.no)) 
-    stop("Design ", design, " unknown!", call. = FALSE)
-  ades  <- .design.props(d.no)
-  dfe   <- .design.df(ades, robust = robust)
-  steps <- ades$steps
-  if (length(n) == 1) {
-    if (is.finite(n)) 
-      n <- nvec(n = n, grps = ades$steps)
-    else
-      n <- rep(Inf, times = steps)
-    if (n[1] != n[length(n)]) {
-      message("Unbalanced design. n(i)=", paste(n, collapse = "/"),
-              " assumed.")
-    }
-  } else {
-    if (length(n) != ades$steps) {
-      stop("Length of n vector must be ", ades$steps, "!")
-    }
-    if (any(n < 1)) 
-      stop("All n(i) have to be >0.")
-  }
-  nc <- sum(1/n)
-  n <- sum(n)
-  se.fctr <- sqrt(ades$bkni * nc)
+  
   if (logscale) {
     if (missing(theta0))  theta0 <- c(0.95, 0.95)
     if (any(theta0 <= 0)) stop("theta0 must be > 0.")
@@ -101,29 +68,95 @@ prob.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1,
     if (missing(theta2))  theta2 <- 1/theta1
     if (any(theta1 <= 0) || any(theta1 > theta2))
       stop("theta1 and/or theta2 not correctly specified.")
-    ltheta1 <- log(theta1)
-    ltheta2 <- log(theta2)
-    ldiff <- log(theta0)
-    se <- CV2se(CV) * se.fctr
   } else {
     if (missing(theta0)) theta0 <- c(0.05, 0.05)
     if (missing(theta1)) theta1 <- c(-0.2, -0.2)
     if (missing(theta2)) theta2 <- -theta1
     if (any(theta1 > theta2))
       stop("theta1 and/or theta2 not correctly specified.")
-    ltheta1 <- theta1
-    ltheta2 <- theta2
-    ldiff <- theta0
-    se <- CV * se.fctr
   }
-  df <- eval(dfe)
-  if (any(df < 1)) 
-    stop("n too small. Degrees of freedom <1!")
+
+  if(missing(CV))  stop("CVs must be given.")
+  else {
+    if(length(CV)==1) CV <- rep(CV,2)
+    if(length(CV)!=2) stop("CV must have 2 elements.")
+    if(any(CV<=0))   stop("CVs must be >0.")
+  }
+  
+  if(missing(n)) stop("Number of subjects must be given.")
+
+  # correlation
+  if (missing(rho))
+    stop("Correlation between the two endpoints must be given!")
+  if (length(rho) != 1)
+    stop("One rho must be given!")
+  if (rho <= -1 || rho >= 1)
+    stop("Correlation must be > -1 and < 1.")
+
+    # design characteristics
+  # check if design is implemented
+  d.no <- .design.no(design)
+  if (is.na(d.no)) stop("Design ",design, " unknown!", call.=FALSE)
+  
+  # design characteristics
+  ades <- .design.props(d.no)
+  # degrees of freedom as expression
+  dfe  <- .design.df(ades, robust=robust)
+  # we use always bkni
+  # step size = no of sequences
+  steps <- ades$steps
+  
+  # handle n = ntotal if scalar else n's of the sequence groups
+  if (length(n) == 1) {
+    # total n given    
+    # for unbalanced designs we divide the ns by ourself
+    # to have only small imbalance (function nvec() from Helper_dp.R)
+    if(is.finite(n)) n <- nvec(n=n, grps=ades$steps) else n <- rep(Inf, times=steps)
+    if (n[1]!=n[length(n)]){
+      message("Unbalanced design. n(i)=", paste(n, collapse="/"), " assumed.")
+    } 
+  } else {
+    if (length(n) != ades$steps) {
+      stop("Length of n vector must be ", ades$steps, "!")
+    }
+    if (any(n<1)) stop("All n(i) have to be >0.")
+  }
+  nc <- sum(1/n)
+  n  <- sum(n)
+  # Cfact*mse gives the variance of the mean difference
+  Cfact <- ades$bkni * nc
+  
+  if (missing(nsims)) {
+    nsims <- 1E5
+    if (t1e) nsims <- 1E6
+  }
+  
+  if (setseed) set.seed(1234567)
+  
+  # 'true' variance-covariance matrix
+  sigma <- matrix(0, nrow=2, ncol=2)
+  if(logscale) {
+    ltheta0 <- log(theta0)
+    ltheta1 <- log(theta1)
+    ltheta2 <- log(theta2)
+    diag(sigma) <- CV2mse(CV)
+    sigma[1,2]  <- sigma[2,1] <- rho*sqrt(sigma[1,1]*sigma[2,2])
+  } else {
+    ltheta0 <- theta0
+    ltheta1  <- theta1
+    ltheta2  <- theta2
+    diag(sigma) <- CV^2 # ??? is this correct?
+    sigma[1,2]  <- sigma[2,1] <- rho*sqrt(sigma[1,1]*sigma[2,2])
+  }
+  # now the calculations are starting
+  # start timer
+  ptm  <- proc.time()
+  df  <- eval(dfe)
   if (t1e) {
     # Calculate Type I Error
     nullsets <- vector("list", 8)
     lim <- 100  # 100 instead of Inf; suffices and avoids potential
-                # optimization problems when using Inf
+    # optimization problems when using Inf
     H_A01 <- c(-lim, log(theta1[1]))
     H_A02 <- c(log(theta2[1]), lim)
     H_C01 <- c(-lim, log(theta1[2]))
@@ -142,9 +175,9 @@ prob.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1,
       # Determine starting values (theta0[1], theta0[2])
       starting <- c(.getStart(H[1, ], lim), .getStart(H[2, ], lim))
       # Perform maximization over intersection nullset H via optim()
-      res <- optim(par = starting, fn = .prob.2TOST.old, se = se, df = df, 
-                   ltheta1 = ltheta1, ltheta2 = ltheta2, rho = rho, 
-                   alpha = alpha, setseed = setseed, eps = 1e-05, 
+      res <- optim(par = starting, fn = .prob.2TOST, alpha = alpha, df = df,
+                   Cfact = Cfact, ltheta1 = ltheta1, ltheta2 = ltheta2, 
+                   sigma = sigma, rho = rho, nsims = nsims,
                    method = "L-BFGS-B", lower = c(H[1, 1], H[2, 1]), 
                    upper = c(H[1, 2], H[2, 2]), control = list(fnscale = -1))
       if (!(res$convergence %in% c(0, 1)))
@@ -163,40 +196,90 @@ prob.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1,
     probs <- cbind(nullsets.label, probs)
     colnames(probs) <- c("Intersection null", "P(Type I Error)", "theta0 #1", 
                          "theta0 #2")
-    if (details)
-      return(probs)
-    else
-      return(max(probs[["P(Type I Error)"]]))
+    prob <- if (details) probs else max(probs[["P(Type I Error)"]])
   } else {
     # Calculate Power
-    fcn.max <- .prob.2TOST.old(ltheta0 = log(theta0), se = se, df = df, 
-                            ltheta1 = ltheta1, ltheta2 = ltheta2, rho = rho, 
-                            alpha = alpha, setseed = setseed)
-    fcn.max
+    prob <- .prob.2TOST(ltheta0 = ltheta0, alpha = alpha, df = df, Cfact = Cfact, 
+                        ltheta1 = ltheta1, ltheta2 = ltheta2, sigma = sigma, 
+                        rho = rho, nsims = nsims)
   }
+  #pwr <- .pwr.2TOST.sim(alpha, df, Cfact, ltheta0, ltheta1, ltheta2, sigma, 
+  #                      rho, nsims)
+  if (details) {
+    cat(nsims, "simulations. Time consumed (secs)\n")
+    print(proc.time()-ptm)
+    cat("\n")
+  }
+  prob
 }
 
-.prob.2TOST.old <- function(ltheta0, se, df, ltheta1, ltheta2, rho, 
-                            alpha = c(0.05, 0.05), setseed = TRUE, eps = 1e-04) {
-  if (setseed)
-    set.seed(12345)
-  tval <- qt(1 - alpha, df)
-  lower <- c(tval[1], -Inf, tval[2], -Inf)
-  upper <- c(Inf, -tval[1], Inf, -tval[2])
-  delta <- c((ltheta0[1] - ltheta1[1]) / se[1],
-             (ltheta0[1] - ltheta2[1]) / se[1],
-             (ltheta0[2] - ltheta1[2]) / se[2],
-             (ltheta0[2] - ltheta2[2]) / se[2])
-  corr <- matrix(1, ncol = 4, nrow = 4)
-  corr[1:2, 3:4] <- corr[3:4, 1:2] <- rho
-  prob <- pmvt(lower = lower, upper = upper, delta = delta, df = df, corr = corr,
-               algorithm = GenzBretz(maxpts = 100000L, abseps = eps))
-  if (attr(prob, which = "msg") != "Normal Completion")
-    warning("pmvt returned message ", attr(prob, which = "msg"), call. = FALSE)
-  prob[1]
-}
+# ---------------------------------------------------------------------------
+# working horse. to be used also in sampleN.2TOST()
+.prob.2TOST <- function(ltheta0, alpha, df, Cfact, ltheta1, ltheta2, sigma, 
+                        rho, nsims)
+{
+  tvals <- qt(1-alpha, df)
+  # cave sqrt() in case of rho<0
+  s2m   <- sigma*Cfact
+  
+  # over chunks of 1E7 if nsims > 1E7 to avoid memory problems
+  chunksize <- 1e7
+  chunks    <- 1
+  nsi       <- nsims
+  if(nsims > chunksize) {
+     chunks <- ceiling(nsims/chunksize)
+     nsi    <- chunksize
+  } 
+  BE <- 0
+  for (iter in 1:chunks){
+    # if chunks*1E7 > nsims then correct nsi to given rest of nsims
+    if(iter==chunks & chunks>1) nsi <- nsims-(chunks-1)*nsi
+    # reserve memory
+    pes  <- matrix(logical(0), nrow=nsi, ncol=2)
+    mses <- matrix(logical(0), nrow=nsi, ncol=2)
+    # next: if directive with rho==0 was only for comparative purposes
+    # no need to have it. multivariate NV and Wishart can handle this case
+    # but retained by reason of an eventual speed gain
+    if (rho==0){
+      # simulate point est. via 2 independen normal distributions
+      pes[ , 1] <- rnorm(n=nsi, mean=ltheta0[1], sd=sqrt(s2m[1,1])) # metric 1, f.i. AUC
+      pes[ , 2] <- rnorm(n=nsi, mean=ltheta0[2], sd=sqrt(s2m[2,2])) # metric 2, f.i. Cmax
+      # simulate mse via 2 independent chi-squared distributions
+      mses[ , 1] <- sigma[1,1]*rchisq(n=nsi, df=df)/df
+      mses[ , 2] <- sigma[2,2]*rchisq(n=nsi, df=df)/df
+    } else {
+      # pes multivariate normal with rho
+      pes <- mvtnorm::rmvnorm(nsi, mean=ltheta0, sigma=s2m)
+      # simulate covariance matrices via Wishart distribution
+      covm      <- stats::rWishart(n=nsi, df=df, Sigma=sigma)/df 
+      mses[, 1] <- covm[1, 1, ]
+      mses[, 2] <- covm[2, 2, ]
+      # take care of memory
+      rm(covm)
+    } 
 
-.getStart.old <- function(H, lim) {
+    BE_test <- function(nu)
+    {
+      # nu = number of the metric
+      hw <- tvals[nu]*sqrt(Cfact*mses[, nu])
+      loCL <- pes[, nu] - hw
+      upCL <- pes[, nu] + hw
+      BE <- loCL >= ltheta1[nu] & upCL <= ltheta2[nu]
+      BE
+    }
+    # make BE decision for both metrics
+    BE_m1 <- BE_test(nu=1)
+    BE_m2 <- BE_test(nu=2)
+    
+    # overall BE 
+    BE <- BE + sum(BE_m1 & BE_m2)
+  } # end over chunks
+  
+  return(BE/nsims)
+  
+} #end function
+
+.getStart <- function(H, lim) {
   if (H[1] <= -lim) {
     return(H[2])
   } else if (H[2] >= lim) {
@@ -205,24 +288,3 @@ prob.2TOST.old <- function(alpha = c(0.05, 0.05), logscale = TRUE, theta1,
     return((H[1] + H[2]) / 2)
   }
 }
-
-# Gives the same numbers but is a bit slower
-#.prob.2TOST.old <- function(ltheta0, se, df, ltheta1, ltheta2, rho,
-#                         alpha = c(0.05, 0.05), setseed = TRUE, eps = 1e-04) {
-#  if (setseed)
-#    set.seed(12345)
-#  tval <- qt(1 - alpha, df)
-#  lower <- c(tval[1], tval[1], tval[2], tval[2])
-#  upper <- c(Inf, Inf, Inf, Inf)
-#  delta <- c((ltheta0[1] - ltheta1[1]) / se[1],
-#             -(ltheta0[1] - ltheta2[1]) / se[1],
-#             (ltheta0[2] - ltheta1[2]) / se[2],
-#             -(ltheta0[2] - ltheta2[2]) / se[2])
-#  corr <- diag(4)
-#  corr[upper.tri(corr)] <- corr[lower.tri(corr)] <- c(-1, rho, -rho, -rho, rho, -1)
-#  prob <- pmvt(lower = lower, upper = upper, delta = delta, df = df, corr = corr,
-#               algorithm = GenzBretz(maxpts = 100000L, abseps = eps))
-#  if (attr(prob, which = "msg") != "Normal Completion")
-#    warning("pmvt returned message ", attr(prob, which = "msg"), call. = FALSE)
-#  prob[1]
-#}
