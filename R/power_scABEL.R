f404f1db2cd1274be14f3e2d7245df799d53ab12
Detlew <detlewlabes@gmx.de>
2019-08-14 16:12:35 +0200

    Trying to introduce argument sdsims

--- R/power_scABEL.R
+++ R/power_scABEL.R
@@ -6,25 +6,32 @@
 #---------------------------------------------------------------------------
 power.scABEL <- function(alpha=0.05, theta1, theta2, theta0, CV, n,   
                          design=c("2x3x3", "2x2x4", "2x2x3"), regulator,
-                         nsims=1E5, details=FALSE, setseed=TRUE)
+                         sdsims = FALSE, nsims=1E5, details=FALSE, setseed=TRUE)
 {
   # design must be checked outside
   desi <- match.arg(design)
   # check regulator
   if (missing(regulator)) regulator <- "EMA"
   reg  <- reg_check(regulator)
-  pwrfun <- "power.scABEL1"
-  if (reg$est_method=="ISC") pwrfun <- "power.scABEL2"
-  # next doesn't function if one or more theta's missing
-  # r <- do.call(pwrfun,
-  #              list(alpha, theta1, theta2, theta0, CV, n, design=desi, reg, 
-  #                   nsims, details, setseed))
+
   if (reg$est_method!="ISC"){
-    # power via key statistic sims with empirical adaptions to obtain
-    # better agreement to sims based on subject data
-    r <- power.scABEL1(alpha, theta1, theta2, theta0, CV, n, design=desi, reg, 
-                       nsims, details, setseed)
+    if (sdsims) {
+      # power via subject data sims
+      # additional to the args of power.scABEL1 / power.scABEL2 
+      # are design_dta, progress
+      r <- power.scABEL.sdsims(alpha, theta1, theta2, theta0, CV, n, design=desi,  
+                               design_dta=NULL, regulator=reg, nsims, details, 
+                               setseed)
+      
+    } else {
+      # power via key statistic sims with empirical adaptions to obtain
+      # better agreement to sims based on subject data
+      r <- power.scABEL1(alpha, theta1, theta2, theta0, CV, n, design=desi, reg, 
+                         nsims, details, setseed)
+      
+    }
   } else {
+    # power via key statistic sims
     r <- power.scABEL2(alpha, theta1, theta2, theta0, CV, n, design=desi, reg, 
                        nsims, details, setseed)
   } 
