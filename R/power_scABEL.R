97113aa3edc98c33c4006e947af52914946b4fb4
Detlew <detlewlabes@gmx.de>
2019-08-15 14:43:41 +0200

    Argument sdsims removed again from power.scABEL()

--- R/power_scABEL.R
+++ R/power_scABEL.R
@@ -6,7 +6,7 @@
 #---------------------------------------------------------------------------
 power.scABEL <- function(alpha=0.05, theta1, theta2, theta0, CV, n,   
                          design=c("2x3x3", "2x2x4", "2x2x3"), regulator,
-                         sdsims = FALSE, nsims=1E5, details=FALSE, setseed=TRUE)
+                         nsims=1E5, details=FALSE, setseed=TRUE)
 {
   # design must be checked outside
   desi <- match.arg(design)
@@ -15,19 +15,10 @@ power.scABEL <- function(alpha=0.05, theta1, theta2, theta0, CV, n,
   reg  <- reg_check(regulator)
 
   if (reg$est_method!="ISC"){
-    if (sdsims) {
-      # power via subject data sims
-      # additional to the args of power.scABEL1 / power.scABEL2 
-      # are the argumets design_dta, progress
-      r <- power.scABEL.sdsims(alpha, theta1, theta2, theta0, CV, n, design=desi,  
-                               design_dta=NULL, regulator=reg, nsims, details, 
-                               setseed)
-    } else {
       # power via key statistic sims with empirical adaptions to obtain
       # better agreement to sims based on subject data
       r <- power.scABEL1(alpha, theta1, theta2, theta0, CV, n, design=desi, reg, 
                          nsims, details, setseed)
-    }
   } else {
     # power via key statistic sims
     r <- power.scABEL2(alpha, theta1, theta2, theta0, CV, n, design=desi, reg, 
